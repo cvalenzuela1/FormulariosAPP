74a0eec65717983ae0cb0e215abe784b6a76ae1b
cvalenzuela1 <101999993+cvalenzuela1@users.noreply.github.com>
2023-04-01 20:24:21 -0300

    First commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidemenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
