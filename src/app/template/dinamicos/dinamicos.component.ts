889dad3e7c800643712ca2bd7a1fc9ae41b50046
cvalenzuela1 <101999993+cvalenzuela1@users.noreply.github.com>
2023-04-13 20:54:17 -0400

    Agregar y eliminar dinámicamente

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Favorito, Persona } from '../interfaces/persona.interface';
 
 @Component({
   selector: 'app-dinamicos',
@@ -10,6 +11,16 @@ export class DinamicosComponent {
   
   @ViewChild("formulario") formulario!: NgForm;
 
+  nuevo_favorito: string = "";
+
+  persona: Persona = {
+    nombre: "Camilo",
+    favoritos: [
+      { id: 1, nombre: "Metal Gear" },
+      { id: 2, nombre: "Death Stranding" }
+    ]
+  }
+
   nombreValido(): boolean {
     return this.formulario?.controls['nombre']?.invalid && this.formulario?.controls['nombre']?.touched;
   }
@@ -17,4 +28,19 @@ export class DinamicosComponent {
   guardar(): void {
     console.log("formulario posteado");
   }
+
+  agregar(): void {
+    const favorito_obj: Favorito = {
+      id: (this.persona.favoritos.length + 1),
+      nombre: this.nuevo_favorito
+    }
+    this.persona.favoritos.push( {...favorito_obj} );
+    this.nuevo_favorito = "";
+  }
+
+  eliminar( index: number ): void {
+    this.persona.favoritos.splice( index, 1 );
+  }
+
+
 }
